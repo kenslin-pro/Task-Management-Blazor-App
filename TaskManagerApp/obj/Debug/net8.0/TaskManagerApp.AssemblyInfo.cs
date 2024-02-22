7e7f126619be616f97dd1e54fc90b68280320d10
kenslin-pro <kenslinerohine@gmail.com>
2024-02-23 01:02:35 +0300

    Added changes

--- TaskManagerApp/obj/Debug/net8.0/TaskManagerApp.AssemblyInfo.cs
+++ TaskManagerApp/obj/Debug/net8.0/TaskManagerApp.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TaskManagerApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3dd703f24481debd3442057eff4fae36d3ddd5fd")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9c53096a83af5496d1ffee144c8ed2a2d8c8632c")]
 [assembly: System.Reflection.AssemblyProductAttribute("TaskManagerApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TaskManagerApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
